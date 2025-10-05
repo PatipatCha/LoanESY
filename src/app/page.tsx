c0d71764be01a93039fa39f9cea706e78699d135
Pat Pat <patipat4@gmail.com>
2025-10-05 16:19:46 +0000

    ปุ่มสลัยภาษา ไม่จำเปน ลบออกไป

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 
 "use client";
 import { LoanCalculator } from '@/components/loan-calculator';
-import { LanguageSwitcher } from '@/components/language-switcher';
 import { useTranslation } from '@/context/language-context';
 
 
@@ -10,9 +9,6 @@ export default function Home() {
 
   return (
     <main className="min-h-screen w-full">
-        <div className="absolute top-4 right-4">
-          <LanguageSwitcher />
-        </div>
       <div className="container mx-auto px-4 py-8 md:py-12">
         <LoanCalculator />
       </div>
